c53b5625659de5ffeec7b2c918bfab03658e8185
Gabriel Macinas <macinasgabriel@gmail.com>
2024-04-29 19:24:00 +0800

    feat: add banner logo

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // webpack config optimize split chunks
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.optimization.splitChunks.cacheGroups = {
+        default: false,
+        vendors: false,
+        // vendor chunk
+        vendor: {
+          name: 'vendor',
+          chunks: 'all',
+          test: /node_modules/,
+        },
+        // common chunk
+        common: {
+          name: 'common',
+          minChunks: 2,
+          chunks: 'all',
+          priority: 10,
+          reuseExistingChunk: true,
+          enforce: true,
+        },
+      };
+    }
+
+    return config;
+  },
+
+};
 
 export default nextConfig;
